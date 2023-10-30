2e1cd5d9a07188b227832419593cca01cbe1270f
marcusmix <tradedomarcus@gmail.com>
2023-10-30 20:47:32 -0300

    feat: vendas controller adicionado com as rotas

--- src/main/java/com/cafedobrasil/cafedobrasil/controller/VendaController.java
+++ src/main/java/com/cafedobrasil/cafedobrasil/controller/VendaController.java
@@ -1,27 +1,67 @@
 package com.cafedobrasil.cafedobrasil.controller;
-
+ 
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 import com.cafedobrasil.cafedobrasil.model.Venda;
 import com.cafedobrasil.cafedobrasil.repository.VendaRepository;
-
+import com.cafedobrasil.cafedobrasil.util.EntidadeException;
+ 
 @RestController
-@RequestMapping("/venda")
+@RequestMapping("/vendas")
 public class VendaController {
-
+ 
     @Autowired
-    private VendaRepository vc;
-
+    private VendaRepository vr;
+ 
     @GetMapping("/")
     public List<Venda> listarVenda(){
-        return vc.findAll();
+        return vr.findAll();
     }
-
-    
-    
+    @PostMapping
+    public Venda salvarVenda(@RequestBody Venda v){
+        return vr.save(v);
+    }
+ 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> excluirProduto(@PathVariable Long id){
+       Venda v = vr.findById(id).orElseThrow(()-> new EntidadeException("venda", id));
+        return ResponseEntity.ok(v);
+    }
+   
+    // caso nao encontre, retorna 200 e o corpo vazio
+    @GetMapping("/{id}")
+    public Venda listarProdutoPorId(@PathVariable Long id){
+        Venda v = vr.findById(id).orElse(null);
+        return v;
+    }
+ 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> listarProdutoPorId(@PathVariable Long id, @RequestBody Venda v ){
+        Venda vAtual = vr.findById(id).orElseThrow(() -> new EntidadeException("Venda", id));
+        if (vAtual.getIdVenda() != null) {
+            v.setIdVenda(id);;
+            vAtual = vr.save(v);
+        }
+        return ResponseEntity.ok(vAtual);
+    }
+ 
+    @ExceptionHandler(EntidadeException.class)
+    public ResponseEntity<String> handleEntidadeNaoEncontrada(EntidadeException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+   
+   
 }
